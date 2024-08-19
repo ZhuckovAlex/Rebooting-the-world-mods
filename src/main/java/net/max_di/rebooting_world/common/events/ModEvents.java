873e261481d0ab86160d61202ed3777b8488fa19
b04ka <daniksla9@gmail.com>
2024-08-19 17:11:07 +0300

    Added new trades for repair template

---
+++ src/main/java/net/max_di/rebooting_world/common/events/ModEvents.java
@@ -0,0 +1,44 @@
+package net.max_di.rebooting_world.common.events;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.max_di.rebooting_world.RTW;
+import net.max_di.rebooting_world.common.items.ModItemsRW;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = RTW.MOD_ID)
+public class ModEvents {
+
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == VillagerProfession.ARMORER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 10+pRandom.nextInt(10)),
+                    new ItemStack(ModItemsRW.REPAIR_TEMPLATE.get()),
+                    3, 1, 0.02f));
+        }
+        if(event.getType() == VillagerProfession.WEAPONSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 10+pRandom.nextInt(10)),
+                    new ItemStack(ModItemsRW.REPAIR_TEMPLATE.get()),
+                    3, 1, 0.02f));
+        }
+        if(event.getType() == VillagerProfession.TOOLSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            trades.get(4).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 10+pRandom.nextInt(10)),
+                    new ItemStack(ModItemsRW.REPAIR_TEMPLATE.get()),
+                    3, 1, 0.02f));
+        }
+    }
+}
